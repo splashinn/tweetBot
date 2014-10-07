a97ec3ddf26638c04d9dbdfb0ace6034eba795b5
Kyle Maune <kmaune71@gmail.com>
2014-10-07 11:40:50 -0700

    add mock config file so user can enter their keys

---
+++ config1.js
@@ -0,0 +1,7 @@
+// put your own twitter app keys here
+module.exports = {
+	consumer_key: 'your consumer key here',
+	consumer_secret: 'your consumer secret here',
+	access_token: 'your access token here',
+	access_token_secret: 'your access token secret here'
+}
