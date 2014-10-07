a5aad19bb60f72e257e388851cc69b7b646537ab
Kyle Maune <kmaune71@gmail.com>
2014-10-07 10:42:12 -0700

    missed # sign in front of search term, fixed now

--- bot.js
+++ bot.js
@@ -5,5 +5,9 @@ var Twit = require('twit');
 var T = new Twit(require('./config.js'));
 
 // url for the search based on the hashtag
-var hashtag = {q: '#javascript', 'rails', count: 10, result_type = 'recent'};
+var hashtag = {q: '#javascript', '#rails', count: 10, result_type = 'recent'};
 
+// this function finds the latest tweet with the hashtags defined and retweets them
+function retweetLatest() {
+
+}
