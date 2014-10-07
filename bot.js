fcf376f71aac2d2d6c040fbeb9bc757e3a896c96
Kyle Maune <kmaune71@gmail.com>
2014-10-07 11:17:21 -0700

    added function for retweeting hash tagged searches

--- bot.js
+++ bot.js
@@ -9,5 +9,28 @@ var hashtag = {q: '#javascript', '#rails', count: 10, result_type = 'recent'};
 
 // this function finds the latest tweet with the hashtags defined and retweets them
 function retweetLatest() {
-
+	T.get('search/tweets', hashtag, function(error, data) {
+		//logs out any errors responses
+		console.log(error, data);
+		// if search request to server has no errors...
+		if (!error) {
+			// then grab the ID of tweet to be retweeted
+			var retweetId = data.statuses[0].id_str;
+			// then tell Twitter we want to retweet it
+			T.post('statuses/retweet/' + retweetId, { }, function(error, response) {
+				if (response) {
+					console.log('Success! Check your bot, it should have retweeted something.');
+				}
+				// if there was an error w/ Twitter call, print it out here
+				if (error) {
+					console.log('There was an error with Twitter: ', error);
+				}
+			})
+		}
+		// if the original search request had an error, print it out here
+		else {
+			console.log('There was an error with you hashtag search: ', error)
+		}
+	});
 }
+
