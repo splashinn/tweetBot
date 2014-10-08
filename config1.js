5363dcaed390a5c6717f94cceff9ff79d368d59b
Kyle Maune <kmaune71@gmail.com>
2014-10-08 09:11:00 -0700

    Update config1.js

--- config1.js
+++ config1.js
@@ -1,7 +1,7 @@
 // put your own twitter app keys here
 module.exports = {
-	consumer_key: 'your consumer key here',
-	consumer_secret: 'your consumer secret here',
-	access_token: 'your access token here',
-	access_token_secret: 'your access token secret here'
+	consumer_key: 'your_consumer_key_here',
+	consumer_secret: 'your_consumer_secret_here',
+	access_token: 'your_access_token_here',
+	access_token_secret: 'your_access_token_secret_here'
 }
